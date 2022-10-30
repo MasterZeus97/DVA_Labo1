28d4404eb747667aa96d11e55cdabb52d0eb86c4
MasterZeus97 <thibault.seem@heig-vd.ch>
2022-10-30 10:06:39 +0100

    Partie 2 done

---
+++ app/src/main/java/com/example/labo1/MainActivityFragment1.kt
@@ -0,0 +1,11 @@
+package com.example.labo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityFragment1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fragment1)
+    }
+}
